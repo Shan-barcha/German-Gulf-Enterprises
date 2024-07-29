7fdefb97d53564351aaac0e7c4ed7a31aa9c28d0
Alishan <alishan11@gmail.com>
2024-07-29 13:54:32 +0500

    remove spaces

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./components/NavBar/navbar";
 import Home from "./pages/home";
 
-
 function App() {
   console.log("location", window.location.pathname);
   const pathname = window.location.pathname;
