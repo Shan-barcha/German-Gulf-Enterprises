98f0a75d19b3fa8a6a0c68e0a1e8ced673130e43
Alishan <alishan11@gmail.com>
2024-07-30 00:52:49 +0500

    home page about section update

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -2,7 +2,7 @@ import CompanyImage from "../../assets/company.jpg";
 
 const About = () => {
   return (
-    <div className="min-h-[900px] relative">
+    <div className="min-h-[900px] relative  overflow-y-hidden">
       <div className="h-[200px] bg-orange-300">
         <div className="my-8 absolute left-[6.5em] top-[4em]">
           <h4 className="text-2xl font-bold">PROUDLY PART OF</h4>
@@ -29,52 +29,42 @@ const About = () => {
             and scrambled it to make a type specimen book.
           </p>
         </div>
-        <div className="flex-1">
-          <img
-            src={CompanyImage}
-            className="w-[500px] h-[100%] absolute top-0 left-[30em]"
-          />
+        <div className="flex-1 w-[100%] mt-[-200px]">
+          <img src={CompanyImage} className="w-[100%] h-[100%]" />
         </div>
+        <div className="flex-1 px-[100px]">
+          <div>
+            <h4 className="text-2xl font-bold my-6">Vision</h4>
+            <p className="text-white">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
 
-        <div className="px-[100px] flex-1">
-          <h4 className="text-2xl font-bold my-6">Vision</h4>
-          <p className="text-white">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-
-          <h4 className="text-2xl font-bold my-6">Mission</h4>
-          <p className="text-white">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-        </div>
-        <p className="bg-green-600 absolute w-full px-2 py-1 bottom-[20%]">
-          {" "}
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text
-        </p>
-        {/* <div className="absolute w-full h-full bottom-[0%]">
-          <div className="text-white font-bold text-2xl md:text-4xl flex flex-nowrap text-nowrap">
-            <h3 className="opacity-100 text-6xl">CI</h3>
-            <h3 className="opacity-20 text-6xl">TYDA</h3>
-            <h3 className="opacity-100 text-6xl">TE</h3>
+            <h4 className="text-2xl font-bold my-6">Mission</h4>
+            <p className="text-white">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
           </div>
-        </div> */}
+
+          <div className="absolute w-fit bottom-[-5%] right-[18%]">
+            <div className="text-white flex flex-nowrap text-nowrap">
+              <h3 className="opacity-100 text-[200px] font-extrabold">CI</h3>
+              <h3 className="opacity-20 text-[200px] font-extrabold">TYDA</h3>
+              <h3 className="opacity-100 text-[200px] font-extrabold">TE</h3>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* <div className="relative w-full h-64 bg-cover bg-center bg-[url('/hero-image.jpg')]">
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="text-white font-bold text-2xl md:text-4xl flex flex-nowrap text-nowrap">
-            <h3 className="opacity-100 text-6xl">CI</h3>
-            <h3 className="opacity-20 text-6xl">TYDA</h3>
-            <h3 className="opacity-100 text-6xl">TE</h3>
-          </div>
-        </div>
-      </div> */}
+      <p className="bg-green-600 absolute w-full px-2 py-1 bottom-[25%]">
+        {" "}
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text
+      </p>
     </div>
   );
 };
